edf1e97b62166f7c64b4f84e16759742056c0075
JoabQH <67796504+JoabQH@users.noreply.github.com>
2025-04-17 14:10:17 -0700

    Create timefixer.h

---
+++ timefixer.h
@@ -0,0 +1,13 @@
+#ifndef __timefixer_H__
+#define __timefixer_H__
+
+#include <app.h>
+#include <Elementary.h>
+
+typedef struct appdata {
+    Evas_Object *win;
+    Evas_Object *conform;
+    Evas_Object *label;
+} appdata_s;
+
+#endif /* __timefixer_H__ */
